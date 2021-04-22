a108aae4755c82d9a826d1c0313a61b26d070109
joselynvcr <n00057871@upn.pe>
2021-04-21 20:53:09 -0500

    nn

--- PROJECT_SOFTWARE/SABOREA/src/PaqueteLog/FormLogin.java
+++ PROJECT_SOFTWARE/SABOREA/src/PaqueteLog/FormLogin.java
@@ -3,38 +3,29 @@ package PaqueteLog;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
-import java.awt.Color;
-import javax.swing.JTextField;
-import javax.swing.JPasswordField;
 import javax.swing.JLabel;
-import java.awt.Font;
-import java.awt.Image;
-
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JPasswordField;
 
-public class FormLogin extends JFrame {
-																															//width height hints	
-	private Image img_logo =new ImageIcon(FormLogin.class.getResource("images/sabore_login.jpg")).getImage().getScaledInstance(90, 90, Image.SCALE_SMOOTH);
-	private Image img_password =new ImageIcon(FormLogin.class.getResource("images/padlock.1.png")).getImage().getScaledInstance(90, 90, Image.SCALE_SMOOTH);
-	private Image img_user =new ImageIcon(FormLogin.class.getResource("images/username.png")).getImage().getScaledInstance(90, 90, Image.SCALE_SMOOTH);
-	private Image img_log_in =new ImageIcon(FormLogin.class.getResource("images/key.2.png")).getImage().getScaledInstance(90, 90, Image.SCALE_SMOOTH);
-	//public Image img_logo = new ImageIcon(FormLogin.class.getResource("images/sabore_login.jpg")).getImage().getScaledInstance(90, 90, Image.SCALE_SMOOTH);
-	
-		
+public class FormLogin extends JFrame implements ActionListener {
+
 	private JPanel contentPane;
-	private JPanel panel;
-	private JPanel panel_1;
-	private JTextField txtUsername;
-	private JPasswordField txtPassword;
-	private JPanel pnlBtnLogin;
+	private JLabel lblNewLabel;
+	private JLabel lblPassword;
+	private JTextField txtUsuario;
 	private JLabel lblLogIn;
-	private JLabel lblX;
-	private JLabel lblIConLogo;
+	private JButton btnIngresar;
+	private JPasswordField jpassClave;
 
 	/**
 	 * Launch the application.
@@ -56,63 +47,71 @@ public class FormLogin extends JFrame {
 	 * Create the frame.
 	 */
 	public FormLogin() {
-		setUndecorated(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 700, 500);
+		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
-		contentPane.setForeground(new Color(47, 79, 79));
-		contentPane.setBackground(new Color(47, 79, 79));
-		contentPane.setBorder(new LineBorder(new Color(255, 140, 0), 4));
+		contentPane.setBackground(Color.PINK);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		panel = new JPanel();
-		panel.setBackground(new Color(255, 255, 255));
-		panel.setBounds(202, 214, 250, 40);
-		contentPane.add(panel);
-		panel.setLayout(null);
+		lblNewLabel = new JLabel("Usuario : ");
+		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 12));
+		lblNewLabel.setBounds(28, 76, 70, 14);
+		contentPane.add(lblNewLabel);
 		
-		txtUsername = new JTextField();
-		txtUsername.setText("Username");
-		txtUsername.setBounds(10, 11, 181, 20);
-		panel.add(txtUsername);
-		txtUsername.setColumns(10);
+		lblPassword = new JLabel("Password : ");
+		lblPassword.setFont(new Font("Arial", Font.BOLD, 12));
+		lblPassword.setBounds(28, 122, 70, 14);
+		contentPane.add(lblPassword);
 		
-		panel_1 = new JPanel();
-		panel_1.setBackground(Color.WHITE);
-		panel_1.setBounds(202, 282, 250, 40);
-		contentPane.add(panel_1);
-		panel_1.setLayout(null);
-		
-		txtPassword = new JPasswordField();
-		txtPassword.setText("Password");
-		txtPassword.setBounds(10, 11, 180, 20);
-		panel_1.add(txtPassword);
-		
-		pnlBtnLogin = new JPanel();
-		pnlBtnLogin.setBackground(new Color(222, 184, 135));
-		pnlBtnLogin.setBounds(202, 349, 256, 58);
-		contentPane.add(pnlBtnLogin);
-		pnlBtnLogin.setLayout(null);
+		txtUsuario = new JTextField();
+		txtUsuario.setBounds(115, 73, 134, 20);
+		contentPane.add(txtUsuario);
+		txtUsuario.setColumns(10);
 		
 		lblLogIn = new JLabel("LOG IN");
-		lblLogIn.setForeground(new Color(47, 79, 79));
-		lblLogIn.setFont(new Font("Arial", Font.BOLD, 16));
-		lblLogIn.setBounds(110, 11, 121, 36);
-		pnlBtnLogin.add(lblLogIn);
+		lblLogIn.setFont(new Font("Arial", Font.BOLD, 15));
+		lblLogIn.setHorizontalAlignment(SwingConstants.CENTER);
+		lblLogIn.setBounds(84, 11, 126, 38);
+		contentPane.add(lblLogIn);
 		
-		lblX = new JLabel("X");
-		lblX.setForeground(Color.WHITE);
-		lblX.setFont(new Font("Comic Sans MS", Font.BOLD, 17));
-		lblX.setHorizontalAlignment(SwingConstants.CENTER);
-		lblX.setBounds(660, 0, 30, 40);
-		contentPane.add(lblX);
+		btnIngresar = new JButton("INGRESAR");
+		btnIngresar.addActionListener(this);
+		btnIngresar.setBounds(84, 172, 109, 36);
+		contentPane.add(btnIngresar);
 		
-		lblIConLogo = new JLabel("");
-		lblIConLogo.setBounds(166, 22, 350, 181);
-		contentPane.add(lblIConLogo);
-		lblIConLogo.setIcon(new ImageIcon(img_logo));
-		getContentPane().add(lblIConLogo);
-		setLocationRelativeTo(null);//si asignamos null es para colocar a la ventana en el centro de la pantalla
+		jpassClave = new JPasswordField();
+		jpassClave.setBounds(119, 120, 130, 20);
+		contentPane.add(jpassClave);
 	}
+	public void actionPerformed(ActionEvent arg0) {
+		if (arg0.getSource() == btnIngresar) {
+			do_btnIngresar_actionPerformed(arg0);
+		}
+	}
+	protected void do_btnIngresar_actionPerformed(ActionEvent arg0) {
+		//para los campos que vamos a ingresar los datos vamos a crear un array de caracteres 
+		//vamos a traer al campo de la contraseña 
+		char [] clave = jpassClave.getPassword();
+		//creamos unva variable de tipo cadena 
+		String claveFinal= new String (clave);
+		if((txtUsuario.getText().equals("Diego")) && (claveFinal.equals("123"))){
+			dispose();
+			JOptionPane.showMessageDialog(null,"Bienvenidos al sistema "," INGRESASTE ", JOptionPane.INFORMATION_MESSAGE);			
+			//llamo a mi ventana principal
+			principal p= new principal();
+			p.setVisible(true);
+		}else{			
+			JOptionPane.showMessageDialog(null,"USUARIO O CONTRASEÑA INCORRECTOS ","ERROR ", JOptionPane.ERROR_MESSAGE);
+			txtUsuario.setText("");
+			jpassClave.setText("");
+			txtUsuario.grabFocus();
+		}
+		
+		
+		
+	}
+	
+	
 }
